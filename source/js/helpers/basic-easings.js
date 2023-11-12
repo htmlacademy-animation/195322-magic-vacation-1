e7485280f29856b0378a3ad4d02e7f3c13a538df
Marina Kaluzhnaya <m.kaluzhnaya@cs.money>
2023-11-12 23:18:21 +0300

    feat: 🎸 add helpers sripts for animations

---
+++ source/js/helpers/basic-easings.js
@@ -0,0 +1,80 @@
+function easeLinear(x) {
+  return x;
+}
+
+function easeInCubic(x) {
+  return x * x * x;
+}
+
+function easeOutCubic(x) {
+  return 1 - Math.pow(1 - x, 3);
+}
+
+function easeInExpo(x) {
+  if (x === 0) {
+    return 0;
+  } else {
+    return Math.pow(2, 10 * x - 10);
+  }
+}
+
+function easeOutExpo(x) {
+  if (x === 1) {
+    return 1;
+  } else {
+    return 1 - Math.pow(2, -10 * x);
+  }
+}
+
+function easeInElastic(x) {
+  const c4 = (2 * Math.PI) / 3;
+
+  if (x === 0) {
+    return 0;
+  } else if (x === 1) {
+    return 1;
+  } else {
+    return Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4);
+  }
+}
+
+function easeOutElastic(x) {
+  const c4 = (2 * Math.PI) / 3;
+
+  if (x === 0) {
+    return 0;
+  } else if (x === 1) {
+    return 1;
+  } else {
+    return Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
+  }
+}
+
+function easeOutBounce(x) {
+  const n1 = 7.5625;
+  const d1 = 2.75;
+
+  if (x < 1 / d1) {
+    return n1 * x * x;
+  } else if (x < 2 / d1) {
+    return n1 * (x -= 1.5 / d1) * x + 0.75;
+  } else if (x < 2.5 / d1) {
+    return n1 * (x -= 2.25 / d1) * x + 0.9375;
+  } else {
+    return n1 * (x -= 2.625 / d1) * x + 0.984375;
+  }
+}
+
+
+const _easings = Object.freeze({
+  easeLinear,
+  easeInCubic,
+  easeOutCubic,
+  easeInExpo,
+  easeOutExpo,
+  easeInElastic,
+  easeOutElastic,
+  easeOutBounce
+});
+
+export default _easings;
